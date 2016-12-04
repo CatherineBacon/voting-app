f3f2ee7f1de0ced0c3f334b6021f2226e45bbf64
catherine <catherine.bacon@gmail.com>
2016-12-04 20:39:50 +0000

    set up partials for use with html

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
+var hbs = require('hbs');
 
 var app = express();
 require('dotenv').load();
@@ -14,12 +15,15 @@ require('./app/config/passport')(passport);
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
+hbs.registerPartials(process.cwd() + '/app/views/partials');
+
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
-app.engine('html', require('hbs').__express);
+app.engine('html', hbs.__express);
 app.set('views', './app/views');
 app.set('view engine', 'html');
+
 
 app.use(session({
 	secret: 'secretClementine',
